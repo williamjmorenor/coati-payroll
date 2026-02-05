88c2d43331e7b1c901b8d0448b4b8247a0be2e2d
William Moreno Reyes CP | MBA <wmoreno@bmogroup.solutions>
2026-02-05 19:08:21 +0000

    chore: Update dev script
    Signed-off-by: William Moreno Reyes CP | MBA <wmoreno@bmogroup.solutions>

--- dev/run.sh
+++ dev/run.sh
@@ -1,7 +1,6 @@
 unset DATABASE_URL
 export LOG_LEVEL=trace
 export DEVELOPMENT=True
+export FLASK_ENV=development
 export FLASK_APP=app:app
-payrollctl database init
-payrollctl database migrate
-payrollctl --debug run --reload
+flask run --debug --reload
